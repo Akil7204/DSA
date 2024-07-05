2fb5acea5c6eb2663b83e406e7982a81ba01b350
Akil <akilakil6874@gmail.com>
2024-07-05 20:07:16 +0530

    reversing a string using recursion

--- Recursion/revString.js
+++ Recursion/revString.js
@@ -1,13 +1,13 @@
 // Write a recursive function to reverse a string.
 
-function revString(str){
-    if(str === ""){
-        return ""
-    } else {
-        return revString(str.substr(1)) + str[0];
-    }
+function revString(str) {
+  if (str <= 1) {
+    return str;
+  }
+
+  return revString(str.substr(1)) + str[0];
 }
 
-const string = "Sanooj";
+const string = "akil";
 
 console.log(revString(string));
